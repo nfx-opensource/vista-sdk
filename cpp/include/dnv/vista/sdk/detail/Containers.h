7fd35d0c18edfaae9029f026aab14422f272d1b6
Ronan F. <ronan.fesselier@etik.com>
2025-12-13 19:16:11 +0100

    feat(cpp): add Codebook and Codebooks classes for VIS codebook management

---
+++ cpp/include/dnv/vista/sdk/detail/Containers.h
@@ -0,0 +1,25 @@
+/**
+ * @file Containers.h
+ * @brief Vista SDK container hashing constants
+ * @details Defines stable hash seed values for Vista SDK containers.
+ */
+
+#pragma once
+
+#include <nfx/Containers.h>
+
+#include <cstdint>
+
+namespace dnv::vista::sdk
+{
+	template <typename T, std::size_t N>
+	using SmallVector = nfx::containers::SmallVector<T, N>;
+
+	inline constexpr std::uint32_t FNV_OFFSET_BASIS_32{ 0x811C9DC5 };
+
+	template <typename T>
+	using HashSet = nfx::containers::FastHashSet<T, std::uint32_t, FNV_OFFSET_BASIS_32>;
+
+	template <typename K, typename V>
+	using HashMap = nfx::containers::FastHashMap<K, V, std::uint32_t, FNV_OFFSET_BASIS_32>;
+} // namespace dnv::vista::sdk
